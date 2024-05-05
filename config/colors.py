0683fb57cd2ea950b76075918b4ede2ae2b7dc69
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2024-05-05 23:47:09 +0000

    Updated media utilities for color and welcome message

--- config/colors.py
+++ config/colors.py
@@ -1,12 +1,10 @@
 COLOR_MAP = {
     'yellow': (255, 255, 0),
     'navy': (0, 0, 128),
-    'red': (255, 0, 0),
     'blue': (0, 0, 255),
     'green': (0, 255, 0),
     'black': (0, 0, 0),
     'white': (255, 255, 255),
-    'purple': (128, 0, 128),
 }
 
 TEAM_COLOR_MAP = {
